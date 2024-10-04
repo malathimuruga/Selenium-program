1777773e082608b0db98976dd69e90462f6437df
malathimuruga <139835225+malathimuruga@users.noreply.github.com>
2024-10-04 14:14:41 +0530

    Update Anagram.java

--- week3day2homeassighnment/Anagram.java
+++ week3day2homeassighnment/Anagram.java
@@ -9,37 +9,29 @@ public class Anagram {
 		 String  text1 = "stops"; 
 		 String  text2 = "potss";
 		 
-		 int charCount = text1.length();
-			
-			System.out.println("Number of chars :"+charCount);
-			
-			
-			if(text1.equals(text2)) {
-				System.out.println("its equals() ");
-			}else {
-				System.out.println("not equals()");
-			}
+		 String  text1 = "stops"; 
+		 String  text2 = "potss";
+		 
+		   if(text1.length()==text2.length()) {
+	        	
+	        	System.out.println("String is an Anagaram");
+	        }else {
+	        	System.out.println("Lengths mismatch String is not an anagram ");
+	        	
+	        }
+		   char[] ch1 = text1.toCharArray();
+	       char[] ch2 = text2.toCharArray();
+	       Arrays.sort(ch1);
+	       Arrays.sort(ch2);
+	       boolean result = Arrays.equals(ch1,ch2);
+	     
+	    		   if (result) {
+	    			   System.out.println("The given strings are anagram");
+					
+				} else {
+					System.out.println("The given strings are not an anagram ");
 
-			char[] ch = text1.toCharArray();
-			for (int i = 0; i < ch.length; i++) {
-				System.out.print(ch[i]);
-			}
-			
-			System.out.println();
-		
-			char[] ch1 = text2.toCharArray();
-			for (int i = 0; i < ch.length; i++) {
-				System.out.print(ch1[i]);
-			}
-			
-	char [] ch.Sort();
-	char[] ch1.sort();
-	
-	
-			
-			
-		
-
+				}
 	}
 
 }
